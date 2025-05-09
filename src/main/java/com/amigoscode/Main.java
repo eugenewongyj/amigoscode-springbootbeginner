73b97137920f3e7a7bfff64bda8bd6b52722f39e
eugenewongyj <eugenewongyj@gmail.com>
2025-05-09 16:45:18 +0800

    14 - JSON For Java

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -20,5 +22,38 @@ public class Main {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet){}
+    //record GreetResponse(String greet){}
+
+    class GreetResponse {
+        private final String greet;
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        // this is required otherwise it will error out
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(greet);
+        }
+    }
 }
